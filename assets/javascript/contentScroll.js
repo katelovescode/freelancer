a0455ff6620044a021b6726924de30f759cf53b7
Kate Donaldson <kate@katelovescode.com>
2019-08-15 18:39:22 -0500

    Finishes mobile section nav

--- assets/javascript/contentScroll.js
+++ assets/javascript/contentScroll.js
@@ -1,29 +1,32 @@
-var forward = document.getElementById('slide-forward');
-var back = document.getElementById('slide-back');
+function contentScroll(scrollableId, childClass) {
+  var width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  var forward = document.querySelector('.slide-forward');
+  var back = document.querySelector('.slide-back');
+  var element = document.querySelector(`#${scrollableId}`);
+  var contributionTypeCount = document.querySelectorAll(`.${childClass}`).length - 1
+  var position = 0;
 
-forward.onclick = function() {
-  var container = document.getElementById('contributions-list');
-  sideScroll(container,'right',25,100,10);
-};
+  window.onresize = function() {
+    width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  }
 
-back.onclick = function() {
-  var container = document.getElementById('contributions-list');
-  sideScroll(container,'left',25,100,10);
-};
+  back.onclick = function() {
+    (position >= width) ? position -= width : position = 0;
+    element.scrollTo({
+      top: 0,
+      left: position,
+      behavior: 'smooth'
+    });
+  }
 
-function sideScroll(element,direction,speed,distance,step) {
-  scrollAmount = 0;
-  var slideTimer = setInterval(function() {
-    if(direction == 'left') {
-      console.log("scrolling left");
-      element.scrollLeft -= step;
-    } else {
-      console.log("scrolling right");
-      element.scrollLeft += step;
-    }
-    scrollAmount += step;
-    if(scrollAmount >= distance) {
-      window.clearInterval(slideTimer);
-    }
-  }, speed)
-};
+  forward.onclick = function() {
+    (position < (width * contributionTypeCount)) ? position += width : position = (width * contributionTypeCount);
+    element.scrollTo({
+      top: 0,
+      left: position,
+      behavior: 'smooth'
+    });
+  }
+}
+
+contentScroll("contributions-list", "contribution-type");
